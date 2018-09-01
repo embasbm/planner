69fe737f7f1f9758c605e764638bcd408437a354
Emba Moussa <emba.moussa@eventinc.de>
2018-09-01 14:42:15 +0200

    chore: configure test helpers

---
+++ spec/rails_helper.rb
@@ -0,0 +1,23 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
