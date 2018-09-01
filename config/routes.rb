15a1540ec676fea996d1ada440d390d86de63af2
Emba Moussa <emba.moussa@eventinc.de>
2018-09-01 15:40:39 +0200

    feat: create version for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  constraints subdomain: 'api' do
-  end
+  constraints subdomain: 'api'
+    api_version(module: 'V1', path: { value: 'v1' }) do
+    end
+  do
 end
