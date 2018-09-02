285539317f5566e33e599e90fcc27be6c9f849eb
Emba Moussa <emba.moussa@eventinc.de>
2018-09-02 23:29:50 +0200

    feat: main logic to parse the json file and insert that data in db
    There an extra explanation about SOLID principle about putting this on a service instead of on the controller and/or model

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 The goal of this project is to write a web API to help select activities to do in Madrid (or any city).
 
+## CONCERNS
+
+After a file is posted, a city with that namefile is creAted. I'm not sure when/how to start with reading/parsing the file and create the nested records: should it be a Service, or just use a private method in Cities_controller?
+
+Decided to go for Service, since reading the file is kind of out of the City scaffold scope.
+
 ## REFERENCE
 embasbm@gmail.com
 
---
+++ app/services/v1/city_service.rb
@@ -0,0 +1,42 @@
+module V1::CityService
+  class << self
+    def create_city_content(city, file)
+      content_file = JSON.parse(file.read)
+      return unless content_file
+      create_activities(city, content_file)
+    end
+
+    def create_activities(city, content_file)
+      content_file.each do |activity_params|
+        opening_hours = activity_params.delete('opening_hours')
+        position = activity_params.delete('latlng')
+        new_activity = Activity.create!(activity_params.merge(city: city))
+
+        create_timetable(new_activity, opening_hours)
+        create_position(new_activity, position)
+      end
+    end
+
+    def create_timetable(activity, opening_hours)
+      opening_hours.each do |day|
+        opens_at = day[1].blank? ? '' : day[1][0].split('-')[0]
+        closes_at = day[1].blank? ? '' : day[1][0].split('-')[1]
+
+        OpeningDay.create!(
+          name: day[0],
+          opens_at: opens_at,
+          closes_at: closes_at,
+          activity: activity
+        )
+      end
+    end
+
+    def create_position(activity, position)
+      Position.create!(
+        latitude: position[0],
+        longitud: position[1],
+        activity: activity
+      )
+    end
+  end
+end
