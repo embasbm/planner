020c40ca7df3958e24b27d93b2ef55e823867d46
Emba Moussa <emba.moussa@eventinc.de>
2018-09-03 13:23:56 +0200

    feat: some tdd for city instance method to check results for fetching activities

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,4 +3,31 @@ class City
   field :name, type: String
 
   embeds_many :activities
+
+  def fetch_activities(category = nil, location = nil, district = nil)
+    data_structure = filter_result_by(category, location, district )
+    result = []
+    data_structure.each do |activity|
+      point = {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [activity.position&.latitude, activity.position&.longitud]
+        },
+        properties: {
+          name: activity.name
+        }
+      }
+      result << point
+    end
+    result
+  end
+
+  def filter_result_by(category, location, district)
+    filtered = activities
+    filtered = filtered.select { |x| x.category.downcase.match(category.downcase) } if category.present?
+    filtered = filtered.select { |x| x.location.downcase.match(location.downcase) } if location.present?
+    filtered = filtered.select { |x| x.district.downcase.match(district.downcase) } if district.present?
+    filtered
+  end
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -6,4 +6,41 @@ RSpec.describe City, type: :model do
   it { is_expected.to have_field(:name).of_type(String) }
 
   it { is_expected.to embed_many(:activities) }
+
+  describe '#fetch_activities' do
+    let!(:city) { create(:city) }
+    context 'when no filter present' do
+      it 'returns all activities' do
+        expect(city.fetch_activities.count).to eq 2
+      end
+    end
+
+    context 'when category filter' do
+      it 'reaturn only that category activities' do
+        expect(city.fetch_activities('shopping').count).to eq 1
+        expect(city.fetch_activities('shopping').first[:properties][:name]).to eq 'El Rastro'
+      end
+    end
+
+    context 'when location filter' do
+      it 'reaturn only that location activities' do
+        expect(city.fetch_activities('shopping').count).to eq 1
+        expect(city.fetch_activities(nil,'outdoors').first[:properties][:name]).to eq 'Palacio Real'
+      end
+    end
+
+    context 'when district filter' do
+      it 'reaturn only that district activities' do
+        expect(city.fetch_activities('shopping').count).to eq 1
+        expect(city.fetch_activities(nil, nil, 'Centro').first[:properties][:name]).to eq 'El Rastro'
+      end
+    end
+
+    context 'when multiple filters' do
+      it 'reaturn only that common activities for those filters' do
+        expect(city.fetch_activities('shopping').count).to eq 1
+        expect(city.fetch_activities('shopping', nil, 'Centro').first[:properties][:name]).to eq 'El Rastro'
+      end
+    end
+  end
 end
